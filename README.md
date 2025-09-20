95f13a2f399c39e5fe42486725fc1c6245aafe69
Noor Tabish Qureshy <ntq@gmail.com>
2025-09-20 20:58:44 +0530

    portfolio update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# NTQ-Portfolio
-This is my personal portfolio
+# Personal Portfolio Website
+A fully responsive personal portfolio website developed using HTML, CSS, and JavaScript
+
+# GET IN TOUCH
+[![Instagram](https://img.shields.io/badge/Instagram-%23E4405F.svg?logo=Instagram&logoColor=white)](https://www.instagram.com/noortabishqureshy/) 
+[![ILinkedIn](https://img.shields.io/badge/LinkedIn-%230077B5.svg?logo=linkedin&logoColor=white)](https://www.linkedin.com/in/noortabishqureshy/)
---
+++ script.js
@@ -0,0 +1,68 @@
+/*add event on element*/
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+/*preloader*/
+
+const preloader = document.querySelector("[data-preaload]");
+const delayDuration = 3000;
+setTimeout(function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+}, delayDuration);
+
+/*navbar toggle*/
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+/*active header when window scroll down to 100px*/
+
+const header = document.querySelector("[data-header]");
+
+const activeElemOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeElemOnScroll);
+
+/*scroll reveal*/
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
+
+const reveal = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+}
+
+for (let i = 0, len = revealDelayElements.length; i < len; i++) {
+  revealDelayElements[i].style.transitionDelay = revealDelayElements[i].dataset.revealDelay;
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
